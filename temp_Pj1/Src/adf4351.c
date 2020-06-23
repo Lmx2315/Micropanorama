c432c74f89112419f89d38dcc2a530b075f3fd0b
lmx2315 <lmx2315@mail.ru>
2020-06-23 15:51:29 +0300

    zzz

--- temp_Pj1/Src/adf4351.c
+++ temp_Pj1/Src/adf4351.c
@@ -63,7 +63,7 @@ void INIT_PLL (reg_ADF4351 *pll)
 	pll->RDIV2							=1;
 	pll->R_COUNTER						=10;
 	pll->DOUB_BUFFER					=0;
-	pll->CP_CURRENT						=1;//7-2.5mA
+	pll->CP_CURRENT						=7;//7-2.5mA
 	pll->LDF							=1;//1 - INT N
 	pll->LDP							=0;//0 - 10 ns
 	pll->PD_POLARITY					=1;//0 - negative 1 -positive
